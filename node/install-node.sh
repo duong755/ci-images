385a1bf82f920b2a3b5556238af00ebd12565671
duong755 <ngo.quang.duong.1100@gmail.com>
2021-07-21 14:28:22 +0700

    Remove install scripts

--- node/install-node.sh
+++ node/install-node.sh
@@ -8,13 +8,13 @@ wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.s
 . ~/.nvm/nvm.sh
 nvm install --lts --default
 NODE_VERSION=$(nvm ls | grep -Po -m 1 '\-\>\s+((iojs\-)?v\d+\.\d+\.\d+)' | grep -Po '(iojs\-)?v\d+\.\d+\.\d+')
-nvm use $NODE_VERSION
+nvm use "$NODE_VERSION"
 
 rm -rf ~/.nvm/.cache ~/.nvm/test ~/.nvm/.git ~/.nvm/.github
 
 npm install -g yarn
 
-ln -sf ~/.nvm/versions/node/${NODE_VERSION}/bin/node /usr/bin/node
-ln -sf ~/.nvm/versions/node/${NODE_VERSION}/bin/npm /usr/bin/npm
-ln -sf ~/.nvm/versions/node/${NODE_VERSION}/bin/npx /usr/bin/npx
-ln -sf ~/.nvm/versions/node/${NODE_VERSION}/bin/yarn /usr/bin/yarn
+ln -sf ~/.nvm/versions/node/"${NODE_VERSION}"/bin/node /usr/bin/node
+ln -sf ~/.nvm/versions/node/"${NODE_VERSION}"/bin/npm /usr/bin/npm
+ln -sf ~/.nvm/versions/node/"${NODE_VERSION}"/bin/npx /usr/bin/npx
+ln -sf ~/.nvm/versions/node/"${NODE_VERSION}"/bin/yarn /usr/bin/yarn
